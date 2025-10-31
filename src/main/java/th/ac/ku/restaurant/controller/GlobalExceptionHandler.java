970ead272b591a5e7479403fa1a72ece2080c12a
Rattapoom Pethputong <celica2548@gmail.com>
2025-10-31 11:56:33 +0700

    finish lab 5

--- src/main/java/th/ac/ku/restaurant/controller/GlobalExceptionHandler.java
+++ src/main/java/th/ac/ku/restaurant/controller/GlobalExceptionHandler.java
@@ -5,6 +5,8 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import jakarta.persistence.EntityNotFoundException;
 
@@ -14,13 +16,17 @@ import java.util.Map;
 @ControllerAdvice
 public class GlobalExceptionHandler {
 
-    @ExceptionHandler(MethodArgumentNotValidException.class)
-    public ResponseEntity<?> handleValidationExceptions(
-            MethodArgumentNotValidException ex) {
+    Logger logger = LoggerFactory.getLogger(GlobalExceptionHandler.class);
 
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<Map<String, String>> handleValidationExceptions(
+            MethodArgumentNotValidException ex) {
         Map<String, String> errors = new HashMap<>();
         ex.getBindingResult().getFieldErrors()
                 .forEach(error -> errors.put(error.getField(), error.getDefaultMessage()));
+
+        logger.error("Input Error: {}", errors);
+
         return new ResponseEntity<>(errors, HttpStatus.BAD_REQUEST);
     }
 
