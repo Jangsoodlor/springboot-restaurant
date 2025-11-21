6ce968d40d2e974f50a7c1204dae8806385c971d
Rattapoom Pethputong <celica2548@gmail.com>
2025-11-21 11:23:03 +0700

    finish lab 9 (oauth)

---
+++ src/main/java/th/ac/ku/restaurant/dto/GoogleAuthRequest.java
@@ -0,0 +1,13 @@
+package th.ac.ku.restaurant.dto;
+
+public class GoogleAuthRequest {
+    public String credential;
+
+    public String getCredential() {
+        return credential;
+    }
+
+    public void setCredential(String credential) {
+        this.credential = credential;
+    }
+}
